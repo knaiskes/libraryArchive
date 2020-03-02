624fff362723e14b688e432bf08b27437427add6
Kiriakos Naiskes <kiriakosnaiskes@gmail.com>
2020-03-02 23:01:28 +0200

    add Author, Tag and Book models

--- libraryArchive/books/models.py
+++ libraryArchive/books/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
-# Create your models here.
+class Author(models.Model):
+    author_id = models.AutoField(primary_key=True)
+    full_name = models.CharField(max_length=50, unique=True)
+    # biography
+
+    def __str__(self):
+        return self.full_name
+
+class Tag(models.Model):
+    name = models.CharField(max_length=20, unique=True)
+
+    def __str__(self):
+        return self.name
+
+class Book(models.Model):
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    tags = models.ManyToManyField(Tag)
+
+    title = models.CharField(max_length=50)
+    publication_date = models.DateField()
+    available = models.BooleanField()
+    language = models.CharField(max_length=15) # could be a list but kiss for now
+    pages = models.IntegerField()
+    in_stock = models.BooleanField()
+    description = models.TextField(max_length=150)
+
+    def __str__(self):
+        return self.title
