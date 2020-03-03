73cb5a88d2156cdf5f6a529f8e75c65b0fe56fd2
Kiriakos Naiskes <kiriakosnaiskes@gmail.com>
2020-03-03 14:40:31 +0200

    add base template and book's templates

--- libraryArchive/libraryArchive/settings.py
+++ libraryArchive/libraryArchive/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'libraryArchive.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
