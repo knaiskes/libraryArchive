14ca9757af969269455a2dbf11bf5482d0dca00f
Kiriakos Naiskes <kiriakosnaiskes@gmail.com>
2020-03-04 10:42:15 +0200

    add urls,templates and views to get authors list and author by id

--- libraryArchive/authors/models.py
+++ libraryArchive/authors/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 
 class Author(models.Model):
-    author_id = models.AutoField(primary_key=True)
     full_name = models.CharField(max_length=50, unique=True)
     # biography
 
--- libraryArchive/authors/views.py
+++ libraryArchive/authors/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Author
 
-# Create your views here.
+def authors(request):
+    authors_list = Author.objects.all()
+    context = {
+        'authors_list': authors_list,
+    }
+    return render(request, 'authors/authors.html', context)
+
+def author_by_id(request, id):
+    author = get_object_or_404(Author, id=id)
+    context = {
+        'author': author,
+    }
+    return render(request, 'authors/author.html', context)
