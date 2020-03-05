921578ca867a228e487c4a0f794a8936e227c47c
Kiriakos Naiskes <kiriakosnaiskes@gmail.com>
2020-03-05 16:27:47 +0200

    increase author's models max_length

--- libraryArchive/authors/models.py
+++ libraryArchive/authors/models.py
@@ -1,8 +1,8 @@
 from django.db import models
 
 class Author(models.Model):
-    full_name = models.CharField(max_length=50, unique=True)
-    biography = models.TextField(max_length=200)
+    full_name = models.CharField(max_length=80, unique=True)
+    biography = models.TextField(max_length=700)
 
     def __str__(self):
         return self.full_name
